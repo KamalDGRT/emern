a11446b1c4917bdf0772e0f58a7904f1f0ad7ff5
KamalDGRT <kamaldgrt@gmail.com>
2021-12-30 23:33:03 +0530

    feat: create and read products works using mongo

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,23 @@
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({
-        message: "Route is working fine!",
-    });
+const Product = require("../models/productModel");
+
+// Create product - only for Admin route
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  // Response
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// Get all products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  // Response object
+  res.status(200).json({
+    success: true,
+    products,
+  });
 };
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ const app = require("./app");
 
 const dotenv = require("dotenv");
 
-// Config
+const connectDatabase = require("./config/database");
 
+// Config
 dotenv.config({
-    path: "backend/config/config.env",
+  path: "backend/config/config.env",
 });
 
+// Connecting to Database
+connectDatabase();
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
