8144ab781c4aa78bf1fbce42c736a9a8b0ec539a
KamalDGRT <kamaldgrt@gmail.com>
2021-12-30 23:53:34 +0530

    feat: updating record works

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,3 +21,29 @@ exports.getAllProducts = async (req, res) => {
     products,
   });
 };
+
+// Update product - only for Admin route
+exports.updateProduct = async (req, res, next) => {
+  // we are using let instead of const because we want to change the values
+  console.log(req.params);
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  // Response
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
