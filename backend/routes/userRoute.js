fe72d4ee8c4754e040552e0bf3a9ef2d4a8a4ca1
KamalDGRT <kamaldgrt@gmail.com>
2022-01-01 20:58:56 +0530

    feat: implemented reset password route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,6 +4,7 @@ const {
   loginUser,
   logout,
   forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
 
 module.exports = router;
