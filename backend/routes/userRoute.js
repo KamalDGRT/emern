b72f745e7019e9757258860dc58ef2b9f7cbef6a
KamalDGRT <kamaldgrt@gmail.com>
2022-01-01 22:12:24 +0530

    feat: get current user details

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,7 +5,10 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserDetails,
 } = require("../controllers/userController");
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -14,5 +17,6 @@ router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
