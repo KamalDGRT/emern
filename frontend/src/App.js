dc38e1820e0645a4cba10177f0ec07093a2dd2ee
KamalDGRT <kamaldgrt@gmail.com>
2022-01-02 18:17:21 +0530

    feat: create components: Header, Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
-import Header from "./component/layout/Header.js";
+import Header from "./component/layout/Header/Header";
+import Footer from "./component/layout/Footer/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  });
+
   return (
     <Router>
       <Header />
+
+      <Footer />
     </Router>
   );
 }
