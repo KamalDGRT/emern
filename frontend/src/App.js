0872a0f75aa911be65d34066bb5fe6432acd541f
KamalDGRT <kamaldgrt@gmail.com>
2022-01-02 17:17:19 +0530

    feat: add the overlay navbar
    - Downgraded react-router-dom and react-scripts
    - Followed this Stackoverflow soln: https://stackoverflow.com/a/70405956/12452457

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./component/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
