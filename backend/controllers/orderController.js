4d9a260ba978b728c735abaf5d552d7d05cd1d72
KamalDGRT <kamaldgrt@gmail.com>
2022-01-02 11:19:41 +0530

    feat: add get a single order info route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,3 +32,20 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+// get Single Order
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
